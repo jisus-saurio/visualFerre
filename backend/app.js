77876f9d789ccf03d45c7f92e6b6ee40f61e4db5
stanlyy <stanleyarce51@gmail.com>
2025-03-06 08:03:28 -0600

    Archivos

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+//Importar todo lo de la libreria express
+
+import express from 'express';
+
+//Creo una constane que es igual a la libreria
+const app = express();
+
+//Exporto la constante para poder usar express en otros Archivos
+export default app;
+
---
+++ backend/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+
+const URI = "mongodb://localhost:27017/ferreteriaEPA"
+
+mongoose.connect (URI) ;
+
+const connection = mongoose.connection;
+
+connection.once("open", () => console.log("DB is onnected"))
+
+
+
+connection.on("disconected", () => console.log("DB on") )
